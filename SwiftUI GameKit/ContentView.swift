facd7a8203d72825a9f1b1b6251fe560d0f155ca
yagrariksa <luckyrama0@gmail.com>
2022-10-13 16:52:19 +0700

    Initial Commit

---
+++ SwiftUI GameKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI GameKit
+//
+//  Created by Daffa Yagrariksa on 13/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI GameKit/SwiftUI_GameKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GameKitApp.swift
+//  SwiftUI GameKit
+//
+//  Created by Daffa Yagrariksa on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GameKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
